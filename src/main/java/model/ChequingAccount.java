873efdc75cb064b4fbae81cf3cc460bba74573eb
waylon wang <waylon.wang17@gmail.com>
2025-12-10 03:11:47 -0700

    chequing account wip

---
+++ src/main/java/model/ChequingAccount.java
@@ -0,0 +1,7 @@
+package model;
+
+public class ChequingAccount extends BankAccount{
+    public ChequingAccount(String accName, int accNumber, double accBalance){
+        super(accName, accNumber, accBalance);
+    }
+}
